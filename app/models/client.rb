40f5b89f31c47a7f1868ddfd27d0d60392ff7e4c
Joseph Hassett <jchassett818@gmail.com>
2019-11-18 17:36:31 -0600

    Fixed client and vet models to has many favorited_vets

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,5 +2,5 @@ class Client < ApplicationRecord
   belongs_to :user
   has_many :pets
   has_many :appointments
-  has_many :favorites
+  has_many :favorited_vets
 end
--- app/models/vet.rb
+++ app/models/vet.rb
@@ -1,5 +1,5 @@
 class Vet < ApplicationRecord
   belongs_to :user
-  has_many :favorites
+  has_many :favorited_vets
   has_many :appointments
 end
