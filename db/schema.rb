86ce9f6276b98f3a3efb562f2af38bff40ba8bb9
omar galindo <omargalindog95@gmail.com>
2019-11-19 15:12:45 -0600

    Adding language column to vet and client table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_002742) do
+ActiveRecord::Schema.define(version: 2019_11_19_210440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_002742) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "languages"
     t.index ["user_id"], name: "index_clients_on_user_id"
   end
 
@@ -85,6 +86,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_002742) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "languages"
     t.index ["user_id"], name: "index_vets_on_user_id"
   end
 
