546a818c22c465786e80af56851338a4413b4345
philippine troussel <philippine.troussel11@gmail.com>
2019-11-19 15:54:06 -0600

    created the seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_201631) do
+ActiveRecord::Schema.define(version: 2019_11_19_214243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_201631) do
   end
 
   create_table "pets", force: :cascade do |t|
-    t.string "type"
+    t.string "pets_type"
     t.string "name"
     t.string "history"
     t.bigint "client_id"
