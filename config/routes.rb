89d9b47bd04128ab2b7179206354041555eb874d
Omar Galindo <53888260+oegg95@users.noreply.github.com>
2019-11-19 14:39:05 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :vet,  only: [:index]
   resources :user do
-    resources :appointment, only: [:new, :create, :show, :delete]
+    resources :appointment, only: [:new, :create, :show, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
