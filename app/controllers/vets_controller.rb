4eb91e2a478b6257f5cb4e3c1055167a1a5fc840
Joseph Hassett <jchassett818@gmail.com>
2019-11-20 15:32:35 -0600

    Fixed alot of stuff -- added controllers, controller actions, views, changed routes again

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -1,7 +1,7 @@
 class VetsController < ApplicationController
 
   def index
-
+    @vets = Vet.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :vets,  only: [:index]
-  resources :users do
-    resources :appointments, only: [:new, :create, :show, :destroy] do
-      resources :pets, only: [:index]
+  resources :vets,  only: [:index] # done
+  resources :clients # done
+  resources :appointments, only: [:new, :create, :show, :destroy] do
+      resources :pets, only: [:index] # not necessary?? should be able to each through @pets where pet.client = client.user = current_user
     end
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
