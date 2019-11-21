7a87d2072e4bcc73b127a46ddd7080e39e8085ba
omar galindo <omargalindog95@gmail.com>
2019-11-21 15:31:42 -0600

    Finished filtering closest vets

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_214243) do
-
+ActiveRecord::Schema.define(version: 2019_11_19_234347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +95,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_214243) do
     t.integer "check_in"
     t.integer "check_out"
     t.integer "rate"
-    t.string "languages"
     t.index ["user_id"], name: "index_vets_on_user_id"
   end
 
