09feaa265a5ae963e4dcc42f4d484df3012f3c26
philippine troussel <philippine.troussel11@gmail.com>
2019-11-21 15:34:45 -0600

    language form

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_214243) do
-
+ActiveRecord::Schema.define(version: 2019_11_19_234347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,13 +89,12 @@ ActiveRecord::Schema.define(version: 2019_11_19_214243) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rate"
     t.string "languages"
     t.float "latitude"
     t.float "longitude"
     t.integer "check_in"
     t.integer "check_out"
-    t.integer "rate"
-    t.string "languages"
     t.index ["user_id"], name: "index_vets_on_user_id"
   end
 
