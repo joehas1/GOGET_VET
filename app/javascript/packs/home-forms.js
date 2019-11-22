b6ca488180fde44eba06fc6aaa22a0c7441f3344
philippine troussel <philippine.troussel11@gmail.com>
2019-11-22 12:02:05 -0600

    finished forms but with no backend

--- app/javascript/packs/home-forms.js
+++ app/javascript/packs/home-forms.js
@@ -2,9 +2,14 @@ const triggerVetButton = document.getElementById("trigger-vet")
 const appointmentModal = document.getElementById("appointment-modal")
 const triggerLanguageNext = document.getElementById("language-next")
 const triggerTypeNext = document.getElementById("type-next")
+const triggerTimeNext = document.getElementById("time-next")
+const triggerGeolocationNext = document.getElementById("geolocation-next")
+
 const languageForm = document.getElementById("language-form")
 const typeForm = document.getElementById("type-form")
 const timeForm = document.getElementById("time-form")
+const geolocationForm = document.getElementById("geolocation-form")
+const symptomForm = document.getElementById("symptom-form")
 
 triggerVetButton.addEventListener("click", event => {
   appointmentModal.style.display = "block"
@@ -19,4 +24,14 @@ triggerLanguageNext.addEventListener("click", event => {
 triggerTypeNext.addEventListener("click", event => {
   typeForm.style.display = "none"
   timeForm.style.display = "block"
+})
+
+triggerTimeNext.addEventListener("click", event => {
+  timeForm.style.display = "none"
+  geolocationForm.style.display = "block"
+})
+
+triggerGeolocationNext.addEventListener("click", event => {
+  geolocationForm.style.display = "none"
+  symptomForm.style.display = "block"
 })
