0973c9fb1edf107c302a5f8d583a7cb2b98c2d0e
Joseph Hassett <jchassett818@gmail.com>
2019-11-25 17:09:10 -0600

    Modified swipe actions left for destroy, right for approve

--- app/javascript/packs/swiping.js
+++ app/javascript/packs/swiping.js
@@ -1,26 +1,69 @@
 import * as Swing from 'swing';
 
-document.addEventListener('DOMContentLoaded', function () {
-    var stack;
 
-    stack = Swing.Stack();
 
-    [].forEach.call(document.querySelectorAll('.stack li'), function (targetElement) {
-        stack.createCard(targetElement);
+ const cards = [].slice.call(document.querySelectorAll('ul li'));
 
-        targetElement.classList.add('in-deck');
-    });
+        // An instance of the Stack is used to attach event listeners.
+        const config = {
+            /**
+             * Invoked in the event of dragmove.
+             * Returns a value between 0 and 1 indicating the completeness of the throw out condition.
+             * Ration of the absolute distance from the original card position and element width.
+             *
+             * @param {number} xOffset Distance from the dragStart.
+             * @param {number} yOffset Distance from the dragStart.
+             * @param {HTMLElement} element Element.
+             * @returns {number}
+             */
+            allowedDirections: [Swing.Direction.LEFT, Swing.Direction.RIGHT, Swing.Direction.UP],
+            throwOutConfidence: (xOffset, yOffset, element) => {
+                const xConfidence = Math.min(Math.abs(xOffset) / element.offsetWidth, 1);
+                const yConfidence = Math.min(Math.abs(yOffset) / element.offsetHeight, 1);
+                var confidence = Math.max(xConfidence, yConfidence);
+                //   var confidenceNew = confidence + 0.8;
+                //   if (confidenceNew > 1) confidenceNew = 1;
+                return confidence;
+            }
+        };
+        const stack = Swing.Stack(config);
 
-    stack.on('throwout', function (e) {
-        console.log(e.target.innerText || e.target.textContent, 'has been thrown out of the stack to the', e.throwDirection, 'direction.');
+        cards.forEach(li => {
+            // Add card element to the Stack.
+            const card = stack.createCard(li);
 
-        e.target.classList.remove('in-deck');
-        window.location.replace('/appointments/new');
-    });
+            // Add event listener for when a card is thrown out of the stack.
+            card.on('throwout', (event) => {
+                event.target.remove()
+            });
+            card.on('throwoutright', (event) => {
+                console.log('Card has been thrown RIGHT.');
+                window.location.replace('/appointments/new');
+            });
+            card.on('throwoutleft', (event) => {
+                console.log('Card has been thrown LEFT.');
+                card.destroy();
+            });
+            card.on('throwoutup', (event) => {
+                console.log('Card has been thrown UP.');
+                window.location.replace('/appointments/new');
+            });
+            card.on('dragmove', (event) => {
+                console.log(event.throwDirection);
+                let offsetX = Math.random() * 500, offsetY = Math.random() * 500;
+                switch (event.throwDirection) {
+                    case Swing.Direction.UP: offsetY = 500; break;
+                    case Swing.Direction.LEFT: offsetX = -500; break;
+                    case Swing.Direction.RIGHT: offsetX = 500; break;
+                    default: break;
+                }
+                if (event.throwOutConfidence > 0.8) {
+                    event.target.style.transition = '0.3s ease-out';
+                    card.throwOut(offsetX, offsetY);
+                }
+            });
+        });
 
-    stack.on('throwin', function (e) {
-        console.log(e.target.innerText || e.target.textContent, 'has been thrown into the stack from the', e.throwDirection, 'direction.');
 
-        e.target.classList.add('in-deck');
-    });
-});
+
+
